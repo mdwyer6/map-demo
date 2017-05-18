13d9781581312baa9b6f18baa59b9dcd4ab05a46
Michael Dwyer <mcdwyer1@gmail.com>
2017-05-18 13:13:59 -0400

    Use css modules

--- src/Location.js
+++ src/Location.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import styles from '../styles/Location.css';
 
 class Location extends Component {
   render() {
     const location = this.props.location;
-    const selectedClass = classnames({'selected': this.props.selected});
+    const selectedClass = classnames({[styles.selected]: this.props.selected});
     return (
       <li onClick={this.props.handleClick.bind(this, location)} className={selectedClass}>{location}</li>
     );
