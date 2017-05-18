75010bfb957f89c0986823bb0df4290f9b04a0de
Michael Dwyer <mcdwyer1@gmail.com>
2017-05-18 11:46:59 -0400

    Save pre ejection application changes

--- src/Location.js
+++ src/Location.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 
 class Location extends Component {
   render() {
     const location = this.props.location;
+    const selectedClass = classnames({'selected': this.props.selected});
     return (
-      <li onClick={this.props.handleClick.bind(this, location)}>{location}</li>
+      <li onClick={this.props.handleClick.bind(this, location)} className={selectedClass}>{location}</li>
     );
   }
 }
